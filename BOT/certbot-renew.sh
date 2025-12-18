051b21fa71d08eaa85b0104d3ce7321204cae657
Osif ungar <osif.e.u@gmail.com>
2025-12-18 13:53:17 +0200

    Update certbot-renew.sh

--- BOT/certbot-renew.sh
+++ BOT/certbot-renew.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# להרצה ידנית: ./certbot-renew.sh
 set -e
 
-DOMAIN="YOUR_DOMAIN"
+DOMAIN="web-production-112f6.up.railway.app"
 EMAIL="you@example.com"
 
-# בקשת תעודה ראשונית (הרץ פעם אחת לפני הפעלת ה-compose במצב מלא)
+# בקשת תעודה ראשונית (הרץ פעם אחת לפני הפעלת ה‑compose במצב מלא)
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
   -d ${DOMAIN} --email ${EMAIL} --agree-tos --no-eff-email" certbot
